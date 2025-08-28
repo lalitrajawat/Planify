dae22c4c37bb9f5a9e44f62d1fac6f1db281c139
PratishthaU <uniyal.pratis27@gmail.com>
2025-08-28 13:03:34 +0530

    created signup page, sighnup style

--- frontend/planify-frontend/src/App.jsx
+++ frontend/planify-frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import LoginPage from "./components/LoginPage";
+import Signup from "./components/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <>
+    <Signup/>
+    </>
   );
 }
-
 export default App;
